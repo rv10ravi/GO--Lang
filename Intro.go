5b0b5d2fdae393aac8fadef802babe284672aedf
Ravi Ray <106743740+rv10ravi@users.noreply.github.com>
2024-02-07 11:43:15 +0530

    Introduction of Go

---
+++ Intro.go
@@ -0,0 +1,29 @@
+// Procedural programming lang
+// Developed - 2007 by Robert Griesemer, Rob Pike, and Ken Thompson at Google but launched in 2009 as an open-source programming language
+
+// Statically typed, concurrent, and garbage-collected programming language 
+// Designed to be simple, efficient, and easy to learn
+
+// Popular choice for building scalable network services, web applications, and command-line tools.
+// Often used for building large-scale distributed systems and high-performance applications.
+
+//key features
+//- Simplicity, Concurrency , Garbage collection , Fast Compile Times , Cross - Platform support , Strong Typing , Large and Growing Community 
+
+//disadvantages
+// - No support for generics 
+// - Not object oriented in conventional sense
+// - Absence of some libraries (UI tool kit)
+// - Limited Object oriented Features (not have full-fledged object-oriented features like inheritance and polymorphism.)
+// - Immature Standard Library
+
+//Sample Code 
+
+package main
+
+import "fmt"
+
+func main(){
+	//print first program
+	fmt.Println("First Program!")
+}
